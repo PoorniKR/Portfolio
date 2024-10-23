da34b7ab84f7a3b5729323b353495695fcef9ef1
Poornima K <164195809+PoorniKR@users.noreply.github.com>
2024-10-23 12:34:15 +0530

    Portfolio

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const popup = document.getElementById("popup");
+const close = document.getElementById("close");
+const videoPopup1 = document.getElementById("videoPopup");
+const videoElement = document.getElementById("videoElement");
+
+popup.addEventListener("click", () => {
+    videoPopup1.style.display = "block";
+    videoElement.play(); 
+});
+
+close.addEventListener("click", () => {
+    videoPopup1.style.display = "none";
+    videoElement.pause(); 
+    videoElement.currentTime = 0; 
+});
